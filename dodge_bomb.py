4bbe30bd7c7531ab08038f33a945b873b6e3ff4f
lily <c0a25260de@edu.teu.ac.jp>
2026-06-16 14:10:54 +0900

    練習１：移動量

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,6 +4,12 @@ import pygame as pg
 
 
 WIDTH, HEIGHT = 1100, 650
+DELTA={
+    pg.K_UP:(0,-5),
+    pg.K_DOWN:(0,+5),
+    pg.K_LEFT:(-5,0),
+    pg.K_RIGHT:(+5,0)
+}
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -14,6 +20,9 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    
+    bb_img=pg.surface((20,20))
+    pg.draw.circle(bb_img,(255,0,0))
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -23,14 +32,19 @@ def main():
         screen.blit(bg_img, [0, 0]) 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        if key_lst[pg.K_UP]:
-            sum_mv[1] -= 5
-        if key_lst[pg.K_DOWN]:
-            sum_mv[1] += 5
-        if key_lst[pg.K_LEFT]:
-            sum_mv[0] -= 5
-        if key_lst[pg.K_RIGHT]:
-            sum_mv[0] += 5
+        #if key_lst[pg.K_UP]:
+         #   sum_mv[1] -= 5
+        #if key_lst[pg.K_DOWN]:
+          #  sum_mv[1] += 5
+        #if key_lst[pg.K_LEFT]:
+         #   sum_mv[0] -= 5
+        #if key_lst[pg.K_RIGHT]:
+         #   sum_mv[0] += 5
+        for key,mv in DELTA.items():
+            if key_lst[key]:
+                sum_mv[0]+=mv[0]
+                sum_mv[1]+=mv[1]
+                
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
         pg.display.update()
